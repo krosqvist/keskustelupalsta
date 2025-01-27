2a2d255370d97b57921d228e860d0f6a0d9ebb0d
Rosqvist Kim S <kirokiro@cubbli22-gold.local>
2025-01-27 16:06:54 +0200

    Add new conversation form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE conversations (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    opening TEXT,
+    user_id INTEGER REFERENCES users
+);
