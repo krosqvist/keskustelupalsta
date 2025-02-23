57a4a48fd68806da6c2449fbab3e50d8f71ec64e
Kim Rosqvist <krosqvist1@gmail.com>
2025-02-23 15:43:03 +0200

    Add actual categories

--- init.sql
+++ init.sql
@@ -1,11 +1,23 @@
 DELETE FROM classes;
 
-INSERT INTO classes (title, value) VALUES ("Kategoria", "Category 1");
-INSERT INTO classes (title, value) VALUES ("Kategoria", "Category 2");
-INSERT INTO classes (title, value) VALUES ("Kategoria", "Category 3");
-INSERT INTO classes (title, value) VALUES ("Kategoria", "Category 4");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Vapaa-aika");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Uutiset");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Teknologia");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Urheilu");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Tiede");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Talous");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Musiikki");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Elokuvat");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "TV");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Pelit");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Kirjat");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Matkailu");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Politiikka");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Koulutus");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Terveys");
+INSERT INTO classes (title, value) VALUES ("Kategoria", "Huumori");
 
-INSERT INTO classes (title, value) VALUES ("Aiheellisuus", "Urgency 1");
-INSERT INTO classes (title, value) VALUES ("Aiheellisuus", "Urgency 2");
-INSERT INTO classes (title, value) VALUES ("Aiheellisuus", "Urgency 3");
-INSERT INTO classes (title, value) VALUES ("Aiheellisuus", "Urgency 4");
+INSERT INTO classes (title, value) VALUES ("Aiheellisuus", "Rento");
+INSERT INTO classes (title, value) VALUES ("Aiheellisuus", "Kysymys");
+INSERT INTO classes (title, value) VALUES ("Aiheellisuus", "Ongelma");
+INSERT INTO classes (title, value) VALUES ("Aiheellisuus", "Mielipide");
